b1106010c71812286c0616f8594616f7bce805c8
3bst0r <johannes.ebster@student.uibk.ac.at>
2021-09-22 11:49:30 +0200

    code format, migrate to python 3, requirements file, password authentication for couchbase

--- cb_to_mongo.py
+++ cb_to_mongo.py
@@ -1,31 +1,36 @@
+import datetime
 import sys
 from threading import Thread
-from couchbase.bucket import Bucket
-import json
+
 import pymongo
-import datetime
+from couchbase.bucket import Bucket
+from couchbase.cluster import Cluster, ClusterOptions, PasswordAuthenticator, ClusterTimeoutOptions
 
-# SERVER = "localhost"
-SERVER = "172.23.100.190"
+SERVER = "dbis-nosql-comparison.uibk.ac.at"
 
 
-def transfer(threads, customers, cbpassword, cbbucket, mongodb, mongotable, offset):
-    batch_size = int(customers) / int(threads)
+def transfer(threads, customers, cbuser, cbpassword, cbbucket, mongodb, mongotable, offset):
+    batch_size = int(customers) // int(threads)
 
     for i in range(0, int(threads)):
         print("process {} started".format(i))
-        new_thread = Thread(target=run_batch, args=(batch_size, i, cbpassword, cbbucket, mongodb, mongotable, offset))
+        new_thread = Thread(target=run_batch,
+                            args=(batch_size, i, cbuser, cbpassword, cbbucket, mongodb, mongotable, offset))
         new_thread.start()
 
 
 def insert_to_mongo(id, doc, mcoll):
     post_id = mcoll.insert_one(doc).inserted_id
-    # print "document {} has been inserted to mongo".format(post_id)
+    print("document {} has been inserted to mongo".format(post_id))
 
 
-def run_batch(size, id, cbpassword, cbbucket, mongodb, mongotable, offset):
-    if cbpassword != "":
-        cb = Bucket("couchbase://{}/{}?operation_timeout=10".format(SERVER, cbbucket), password=cbpassword)
+def run_batch(size, id, cbuser, cbpassword, cbbucket, mongodb, mongotable, offset):
+    if cbpassword != "" and cbuser != "":
+        cluster = Cluster("couchbase://{}".format(SERVER),
+                          ClusterOptions(authenticator=PasswordAuthenticator(cbuser, cbpassword),
+                                         timeout_options=ClusterTimeoutOptions(
+                                             config_total_timeout=datetime.timedelta(seconds=10))))
+        cb = cluster.bucket(cbbucket)
     else:
         cb = Bucket("couchbase://{}/{}?operation_timeout=10".format(SERVER))
 
@@ -54,6 +59,7 @@ def run_batch(size, id, cbpassword, cbbucket, mongodb, mongotable, offset):
 
 threads = 20
 customers = 10000000
+cbuser = ""
 cbpassword = ""
 cbbucket = "bucket-1"
 mongodb = "soe"
@@ -67,6 +73,8 @@ for i, item in enumerate(sys.argv):
         customers = sys.argv[i + 1]
     elif item == "-cbpassword":
         cbpassword = sys.argv[i + 1]
+    elif item == "-cbuser":
+        cbuser = sys.argv[i + 1]
     elif item == "-cbbucket":
         cbbucket = sys.argv[i + 1]
     elif item == "-mongodb":
@@ -76,4 +84,4 @@ for i, item in enumerate(sys.argv):
     elif item == "-offset":
         offset = sys.argv[i + 1]
 
-transfer(threads, customers, cbpassword, cbbucket, mongodb, mongotable, offset)
+transfer(threads, customers, cbuser, cbpassword, cbbucket, mongodb, mongotable, offset)
